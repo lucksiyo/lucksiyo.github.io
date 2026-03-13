1235c4a3e06c968fd66e0e13885b646c68d1e8d4
fj tria <fjtria@protonmail.com>
2026-03-12 22:54:56 -0700

    fixing 404 on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
@@ -7,7 +7,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='process.env.PUBLIC_URL'>
       <App />
     </BrowserRouter>
   </StrictMode>,
